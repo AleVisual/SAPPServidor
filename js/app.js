7fd7c402a443842bfb13900a468433f051c877e0
AleVisual <pc_ale44@outlook.com>
2026-04-12 15:51:41 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    ============================================================ */
 
 // 1. Configuración Global
-const API_URL = 'https://commands-motion-dimension-trinity.trycloudflare.com'; //Siempre colocar la URL de Cloudflared
+const API_URL = 'https://fine-micro-thy-hygiene.trycloudflare.com'; //Siempre colocar la URL de Cloudflared
 const TOKEN_KEY = 'auth_token';
 const USER_KEY = 'user_info';
 
